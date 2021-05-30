ac8ec388e35662c1050a72b53cfd795f593319c8
Ethan Lee <eyechan01@gmail.com>
2021-05-29 23:03:47 -0400

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
@@ -14,8 +13,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
+        <p>Hi this monke bridge</p>
       </header>
     </div>
   );
