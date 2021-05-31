8bc3005cd30a592db249b16f9cd646cfb270c5f8
Ethan Lee <eyechan01@gmail.com>
2021-05-30 21:04:43 -0400

    client-side socket

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import Template from './template';
+import Template from './components/Template';
 
 
 function App() {
